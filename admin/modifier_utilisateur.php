8923ea40c2a29e10e6900cbc09f97a491f59479d
eyagasmi <gasmi.eya@yahoo.com>
2025-05-16 21:16:26 +0200

    Last commit

--- admin/modifier_utilisateur.php
+++ admin/modifier_utilisateur.php
@@ -2,13 +2,16 @@
 require_once '../includes/db.php';
 session_start();
 
+
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['id']) && $_SESSION['role'] === 'admin') {
-    $id = $_POST['id'];
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $email = $_POST['email'];
-    $adresse = $_POST['adresse'];
-    $telephone = $_POST['num_telephone'];
+    $id         = $_POST['id'];
+    $nom        = $_POST['nom'];
+    $prenom     = $_POST['prenom'];
+    $email      = $_POST['email'];
+    $adresse    = $_POST['adresse'];
+    $telephone  = $_POST['num_telephone'];
+    
 
     if (!empty($_POST['mot_de_passe'])) {
         $mdp = password_hash($_POST['mot_de_passe'], PASSWORD_DEFAULT);
